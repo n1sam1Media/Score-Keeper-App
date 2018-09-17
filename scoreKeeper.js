78c608146b452266bbd67077c65a0791f321ff78
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-09-17 22:16:51 +0200

    Add event listener to an input

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -2,6 +2,10 @@
 var gameOver = false;
 var winningScore = 5;
 
+//Selecting input and p span
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
+
 //Player One Button selection
 var p1Button = document.querySelector("#p1");
 var p1Display = document.querySelector("#p1Display");
@@ -14,6 +18,12 @@ var p2Score = 0;
 
 //Selecting reset button
 var resetButton = document.getElementById("reset");
+
+//Event Listener for changing playing to value
+numInput.addEventListener("change", function() {
+  winningScoreDisplay.textContent = numInput.value;
+  winningScore = Number(numInput.value);
+});
 
 //Event Listener for player one
 p1Button.addEventListener("click", function() {
