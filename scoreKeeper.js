af3de2a80115d3aead3d75ed37891828ba3bab96
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-09-17 19:43:04 +0200

    First commit

---
+++ scoreKeeper.js
@@ -0,0 +1,35 @@
+//Limiting number when clicking buttons
+var gameOver = false;
+var winningScore = 5;
+
+//Player One Button selection
+var p1Button = document.querySelector("#p1");
+var p1Display = document.querySelector("#p1Display");
+var p1Score = 0;
+
+//Player Two Button selection
+var p2Button = document.querySelector("#p2");
+var p2Display = document.querySelector("#p2Display");
+var p2Score = 0;
+
+//Event Listener for player one
+p1Button.addEventListener("click", function() {
+  if(!gameOver) {
+    p1Score++;
+    if(p1Score === winningScore) {
+      gameOver = true;
+    }
+    p1Display.textContent = p1Score;
+  }
+});
+
+//Event Listener for player two
+p2Button.addEventListener("click", function() {
+  if(!gameOver) {
+    p2Score++;
+    if(p2Score === winningScore) {
+      gameOver = true;
+    }
+    p2Display.textContent = p2Score;
+  }
+});
