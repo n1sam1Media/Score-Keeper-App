dc55516c458fb2b6316af3b2eb47298fb58c5811
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-09-17 20:29:41 +0200

    -coded the Reset button

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -12,6 +12,9 @@ var p2Button = document.querySelector("#p2");
 var p2Display = document.querySelector("#p2Display");
 var p2Score = 0;
 
+//Selecting reset button
+var resetButton = document.getElementById("reset");
+
 //Event Listener for player one
 p1Button.addEventListener("click", function() {
   if(!gameOver) {
@@ -34,4 +37,15 @@ p2Button.addEventListener("click", function() {
     }
     p2Display.textContent = p2Score;
   }
+});
+
+//Event Listener for Reset Button
+resetButton.addEventListener("click", function() {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
 });
